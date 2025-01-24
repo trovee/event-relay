e20e81d8b2a666e4aa3dc8e3710436b17aaff95b
Serhan KÜRKÇÜOĞLU <serhan.kurkcuoglu@gmail.com>
2025-01-24 21:31:28 +0300

    feat: implement event mapper system
    - Add detailed PHPDoc blocks to EventMapperInterface
    - Implement AbstractEventMapper with flexible mapping system
    - Add event name and data key mapping functionality

--- src/Abstracts/AbstractEventMapper.php
+++ src/Abstracts/AbstractEventMapper.php
@@ -6,6 +6,16 @@ use Trovee\EventRelay\Contracts\{EventInterface, EventMapperInterface};
 
 abstract class AbstractEventMapper implements EventMapperInterface
 {
+    /**
+     * @var array<string, string>
+     */
+    protected array $eventNameMap = [];
+
+    /**
+     * @var array<string, string>
+     */
+    protected array $dataKeyMap = [];
+
     public function map(EventInterface $event): array
     {
         return [
@@ -15,5 +25,40 @@ abstract class AbstractEventMapper implements EventMapperInterface
         ];
     }
 
-    abstract public function mapEventData(string $eventName, array $data): array;
+    public function getEventName(EventInterface $event): string
+    {
+        return $this->eventNameMap[$event->getName()] ?? $event->getName();
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     * @return array<string, mixed>
+     */
+    public function mapEventData(string $eventName, array $data): array
+    {
+        $mappedData = [];
+        
+        foreach ($data as $key => $value) {
+            $mappedKey = $this->dataKeyMap[$key] ?? $key;
+            $mappedData[$mappedKey] = $value;
+        }
+
+        return $mappedData;
+    }
+
+    /**
+     * @param array<string, string> $map
+     */
+    protected function setEventNameMap(array $map): void
+    {
+        $this->eventNameMap = $map;
+    }
+
+    /**
+     * @param array<string, string> $map
+     */
+    protected function setDataKeyMap(array $map): void
+    {
+        $this->dataKeyMap = $map;
+    }
 } 
